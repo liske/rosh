bc9410e58d600cb3dbf0c09ab38a9cf753b3309f
Thomas Liske <thomas@fiasko-nw.net>
2023-11-04 00:13:12 +0100

    commands: split commands by family (AF_INET6 vs. AF_INET), but reuse code if possible

--- rosh/commands/show/ip/neighbour/__init__.py
+++ rosh/commands/show/ip/neighbour/__init__.py
@@ -1,48 +1,12 @@
-from rosh.commands import RoshCommand
-from rosh.completer import link_completer
-from rosh.output import RoshOutputTable
+from socket import AF_INET
 
-class RoshShowIpNeighbourCommand(RoshCommand):
+from rosh.commands.show.ipv6.neighbour import RoshShowIpv6NeighbourCommand
+
+class RoshShowIpNeighbourCommand(RoshShowIpv6NeighbourCommand):
     def __init__(self, rosh):
-        super().__init__(rosh, link_completer)
+        super().__init__(rosh)
+        self.family = AF_INET
 
-    def handler(self, cmd, *args):
-        if len(args) == 0:
-            self.handler_brief()
-        elif len(args) == 1:
-            self.handler_iface(args[0])
-
-    def dump_neigh(self, **filter):
-        tbl = RoshOutputTable()
-        tbl.field_names = ['dst', 'lladdr', 'ifname', 'flags', 'state']
-        tbl.align['dst'] = 'l'
-        tbl.align['ifname'] = 'l'
-        tbl.sortby = 'dst'
-
-        for neigh in self.rosh.ipr.get_neighbours(**filter):
-            tbl.add_row([
-                neigh.get_attr('NDA_DST'),
-                neigh.get_attr('NDA_LLADDR'),
-                neigh['ifindex'],
-                neigh['flags'],
-                neigh['state']
-            ])
-        print(tbl)
-
-    def handler_brief(self):
-        self.dump_neigh()
-
-    def handler_iface(self, ifname):
-        self.dump_neigh(ifname=ifname)
-
-    def validate(self, cmd, args):
-        if len(args) > 1:
-            return (1, "to many parameters (>1)")
-
-        if len(args) == 1 and not args[0] in link_completer.get_links():
-            return (0, f"{args[0]} does not exist")
-
-        return (None, None)
 
 is_rosh_command = True
 rosh_command = RoshShowIpNeighbourCommand
---
+++ rosh/commands/show/ipv6/neighbour/__init__.py
@@ -0,0 +1,52 @@
+from socket import AF_INET6
+
+from rosh.commands import RoshCommand
+from rosh.completer import link_completer
+from rosh.output import RoshOutputTable
+
+class RoshShowIpv6NeighbourCommand(RoshCommand):
+    def __init__(self, rosh):
+        super().__init__(rosh, link_completer)
+        self.family = AF_INET6
+
+    def handler(self, cmd, *args):
+        if len(args) == 0:
+            self.handler_brief()
+        elif len(args) == 1:
+            self.handler_iface(args[0])
+
+    def dump_neigh(self, **filter):
+        tbl = RoshOutputTable()
+        tbl.field_names = ['dst', 'lladdr', 'ifname', 'flags', 'state']
+        tbl.align['dst'] = 'l'
+        tbl.align['ifname'] = 'l'
+        tbl.sortby = 'dst'
+
+        for neigh in self.rosh.ipr.get_neighbours(family=self.family, **filter):
+            tbl.add_row([
+                neigh.get_attr('NDA_DST'),
+                neigh.get_attr('NDA_LLADDR'),
+                neigh['ifindex'],
+                neigh['flags'],
+                neigh['state']
+            ])
+        print(tbl)
+
+    def handler_brief(self):
+        self.dump_neigh()
+
+    def handler_iface(self, ifname):
+        self.dump_neigh(ifname=ifname)
+
+    def validate(self, cmd, args):
+        if len(args) > 1:
+            return (1, "to many parameters (>1)")
+
+        if len(args) == 1 and not args[0] in link_completer.get_links():
+            return (0, f"{args[0]} does not exist")
+
+        return (None, None)
+
+
+is_rosh_command = True
+rosh_command = RoshShowIpv6NeighbourCommand
