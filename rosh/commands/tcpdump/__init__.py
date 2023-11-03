25e8dc864e7e7807149aaf8200011f356fbfa606
Thomas Liske <thomas@fiasko-nw.net>
2023-11-04 00:13:12 +0100

    commands: add tcpdump wrapper

---
+++ rosh/commands/tcpdump/__init__.py
@@ -0,0 +1,13 @@
+import shutil
+
+from rosh.commands import RoshSystemCommand
+
+
+tcpdump_exe = shutil.which('tcpdump')
+
+class RoshTcpdumpCommand(RoshSystemCommand):
+    def __init__(self, rosh):
+        super().__init__(rosh, tcpdump_exe)
+
+is_rosh_command = tcpdump_exe is not None
+rosh_command = RoshTcpdumpCommand
