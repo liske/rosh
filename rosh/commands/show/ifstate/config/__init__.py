2796485fe2254fc02959fbafcb6f73e04acc63f1
Thomas Liske <thomas@fiasko-nw.net>
2023-12-26 23:30:11 +0100

    commands: add `show ifstate config` command

---
+++ rosh/commands/show/ifstate/config/__init__.py
@@ -0,0 +1,55 @@
+from prompt_toolkit import print_formatted_text as print
+from prompt_toolkit.completion import WordCompleter
+import shutil
+
+from rosh.commands import RoshCommand
+from rosh.commands.ifstatecli import ifstatecli_exe
+
+try:
+    import pygments
+    from pygments.lexers import YamlLexer
+    from prompt_toolkit.formatted_text import PygmentsTokens
+
+    # pygements imported successfully
+    has_pygements = 1
+except ImportError:
+    # no pygements available
+    has_pygements = 0
+
+
+class RoshShowIfstateConfigCommand(RoshCommand):
+    description = "show ifstate's config file"
+
+    def handler(self, cmd, *args):
+        config_args = []
+
+        config_file = self.rosh.config['command.ifstatecli'].get('config_file')
+
+        if not config_file:
+            config_file = '/etc/ifstate/config.yml'
+
+        try:
+            with open(config_file) as fh:
+                code = fh.read().encode('utf8')
+        except IOError as ex:
+            print(f'could not read file: {ex}')
+            return
+        except UnicodeEncodeError as ex:
+            print(f'encoding problem: {ex}')
+            return
+
+        if has_pygements:
+            tokens = list(pygments.lex(code, lexer=YamlLexer()))
+            print(PygmentsTokens(tokens))
+        else:
+            print(code)
+
+    def validate(self, cmd, args):
+        if len(args) > 0:
+            return (0, "no parameters allowed")
+
+        return (None, None)
+
+
+is_rosh_command = ifstatecli_exe is not None
+rosh_command = RoshShowIfstateConfigCommand
